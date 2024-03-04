ea2f0b1d8f315fe6c84ae43cc4f4a000516ce215
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2024-03-04 21:11:21 +0100

    init structure

---
+++ UKT_Project/src/ukt/ui/Main.java
@@ -0,0 +1,25 @@
+package ukt.ui;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Main extends JFrame {
+
+	public Main(String title) {
+		super(title);
+		setSize(1000, 500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new Main("UKT").setVisible(true);
+			}
+
+		});
+	}
+
+}
