a6559bf6ba3c0000baf7fb0776da83954231bafb
RDel-Medico <redelmedico@gmail.com>
2024-03-12 15:36:26 +0100

    Basic json conversion to object with printing

--- UKT_Project/src/ukt/fc/Interface.java
+++ UKT_Project/src/ukt/fc/Interface.java
@@ -2,20 +2,56 @@ package ukt.fc;
 
 public class Interface {
 	
-	public enum Status{
+	public enum Direction {
 		IN,
-		OUT
+		OUT,
+		INOUT
 	}
 
-	private int id;
+	private String id;
 	private String name;
 	private boolean enabled;
-	private Status direction;
+	private Direction direction;
 	
-	public Interface(int id, String name) {
+	public Interface(String id, String name) {
 		this.id = id;
 		this.name = name;
 		this.enabled = true;
-		this.direction = Status.IN;
+		this.direction = Direction.IN;
+	}
+	
+	public void setEnable (Boolean enable) {
+		this.enabled = enable;
+	}
+	
+	public void setDirection (Direction dir) {
+		this.direction = dir;
+	}
+	
+	public void setDirection (String dir) {
+		if (dir.equals("output")) {
+			this.direction = Direction.OUT;
+		} else if (dir.equals("input")) {
+			this.direction = Direction.IN;
+		} else {
+			this.direction = Direction.INOUT;
+		}
+	}
+	
+	public String directionToString (Direction d) {
+		switch (d) {
+		case IN:
+			return "input";
+		case OUT:
+			return "output";
+		case INOUT:
+			return "input/output";
+		default:
+			return "";
+		}
+	}
+	
+	public void print() {
+		System.out.println("			L'interface d'id : " + this.id + " à pour nom : " + this.name + " et est " + (this.enabled ? "active" : "désactive") + " de direction " + this.directionToString(this.direction));
 	}
 }
--- UKT_Project/src/ukt/fc/Node.java
+++ UKT_Project/src/ukt/fc/Node.java
@@ -4,15 +4,38 @@ import java.util.ArrayList;
 
 public class Node {
 	
-	private int id;
+	private String id;
 	private String name;
 	private ArrayList<Interface> interfaces;
 	private ArrayList<Property> properties;
 	
-	public Node(int id, String name) {
+	public Node(String id, String name) {
 		this.id = id;
 		this.name = name;
 		interfaces = new ArrayList<>();
 		properties = new ArrayList<>();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void setInterfaces(ArrayList<Interface> interfaces) {
+		this.interfaces = (ArrayList<Interface>)interfaces.clone();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void setProperties(ArrayList<Property> properties) {
+		this.properties = (ArrayList<Property>)properties.clone();
+	}
+	
+	public void print() {
+		System.out.println("	Le noeud d'id : " + this.id + " à pour nom : " + this.name);
+		System.out.println("	Les interfaces de ce noeud sont : ");
+		for (Interface i : this.interfaces) {
+			i.print();
+		}
+		System.out.println();
+		System.out.println("	Les propriétés de ce noeud sont : ");
+		for (Property p : this.properties) {
+			p.print();
+		}
 	}
 }
