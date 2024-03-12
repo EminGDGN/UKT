7931e18641b7244a6bf785402e2c796e7aaa0bb6
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2024-03-12 12:40:24 +0100

    add first graphical components to ihm

--- UKT_Project/src/ukt/ui/Main.java
+++ UKT_Project/src/ukt/ui/Main.java
@@ -1,15 +1,36 @@
 package ukt.ui;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Main extends JFrame {
 
-	public Main(String title) {
-		super(title);
+	public Main() {
+
+		super("UKT");
+
+		// Set window size
 		setSize(1000, 500);
+
+		// Make the window appear in the middle of the screen
+		setLocationRelativeTo(null);
+
+		// Exit on closing window
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// Set layout
+		this.getContentPane().setLayout(new BorderLayout(10,10));
+
+		// Set menu bar
+		setJMenuBar(new UKTMenuBar(this));
+
+		// Add center split pane
+		//add(new UKTSplitPane(this), BorderLayout.CENTER);
+
+		// Add bottom tool bar
+		//add(new UKTBottomBar(this), BorderLayout.SOUTH);
 	}
 
 	public static void main(String[] args) {
@@ -17,7 +38,7 @@ public class Main extends JFrame {
 
 			@Override
 			public void run() {
-				new Main("UKT").setVisible(true);
+				new Main().setVisible(true);
 			}
 
 		});
--- UKT_Project/src/ukt/ui/UKTMenuBar.java
+++ UKT_Project/src/ukt/ui/UKTMenuBar.java
@@ -1,25 +1,71 @@
 package ukt.ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
 public class UKTMenuBar extends JMenuBar {
+	
+	private File selectedGraphFile;
+	private JFrame parent;
 
-	private JMenu fileMenu, editMenu, helpMenu;
-	private JMenuItem
-
-	public UKTMenuBar() {
-
-		// File menu
-		fileMenu = new JMenu("File");
+	public UKTMenuBar(JFrame p) {	
 		
+		parent = p;
 
 		// File menu
-		editMenu = new JMenu("Edit");
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic('F');
 
-		// File menu
-		helpMenu = new JMenu("Help");
+		JMenuItem loadGraphFileMenu = new JMenuItem("Load graph file");
+		loadGraphFileMenu.setMnemonic('L');
+		loadGraphFileMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, KeyEvent.CTRL_DOWN_MASK));
+		loadGraphFileMenu.addActionListener(this::loadGraphFileMenuListener);
+		fileMenu.add(loadGraphFileMenu);
 
+		fileMenu.addSeparator();
+
+		JMenuItem exitFileMenu = new JMenuItem("Exit");
+		exitFileMenu.setMnemonic('x');
+		exitFileMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, KeyEvent.ALT_DOWN_MASK));
+		exitFileMenu.addActionListener(this::exitFileMenuListener);
+		fileMenu.add(exitFileMenu);
+
+		// Adding menu
 		add(fileMenu);
+	}
+	
+	public boolean isGraphFileLoaded() {
+		return selectedGraphFile==null;
+	}
+	
+	public File getLoadedGraphFile() {
+		return selectedGraphFile;
+	}
+
+	private void loadGraphFileMenuListener(ActionEvent e) {
+		JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		fileChooser.setDialogTitle("Select a graph in json format");
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("json", "JSON");
+		fileChooser.addChoosableFileFilter(filter);
+		int result = fileChooser.showOpenDialog(null);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			selectedGraphFile = fileChooser.getSelectedFile();
+			System.out.println(fileChooser.getSelectedFile().getPath());
+		}
+	}
+
+	private void exitFileMenuListener(ActionEvent e) {
+		System.exit(0);
 	}
 }
